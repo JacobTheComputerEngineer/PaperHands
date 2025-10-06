12aebbaba466bfcab7ceae97f11ce00696bc91e5
Eric Yung <94202489+EricYung02@users.noreply.github.com>
2025-10-06 06:56:05 -0400

    implement final game management function

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from . import account, game
 
 class DB:
     
-    def userDB(self, db: MongoClient):
+    def DB(self, db: MongoClient):
         self.db = db
         self.userdb = db["users"]
         self.gamedb = db["games"]
@@ -16,7 +16,7 @@ class DB:
         
     def addUserToGame(self, user: account.UserAccount, game: game.Game):
         #TODO: add error checking, maybe use _id instead of id for both user and gameID
-        user = self.userDB.find_one({"username": user.username})
+        user = self.userdb.find_one({"username": user.username})
         self.userdb.update_one({
             "username": user.username,
             "%addToSet":{"games": game.gameID}
@@ -28,13 +28,22 @@ class DB:
             })
         
     def removeUserFromGame(self, user: account.UserAccount, game: game.Game):
-        
+        #TODO: add error checking for no user found in DB
         self.gamedb.update_one({
             "gameID": game.gameID,
             "%pull":{"users": user.username}
             })
         
-        self.userDB.update_one({
+        self.userdb.update_one({
             "username": user.username,
             "%pull": {"games": game.gameID}
         })
+        
+    def removeGame(self, game: game.Game):
+        
+        #get players and remove all players if any left
+        for users in game.players:
+            self.removeUserFromGame(users, game)
+            
+        #remove leftover game
+        self.gamedb.delete_one({"gameID": game.gameID})
